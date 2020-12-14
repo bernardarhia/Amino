21c4727912b87131a37695a15c302a302c04cc15
bernardarhia <megamindcooperations@gmail.com>
2020-12-14 09:37:40 +0000

    fixed styling problem and responsiveness

--- scripts/animate.js
+++ scripts/animate.js
@@ -1,16 +1,30 @@
+let smoothScroll = new scrollToSmooth("a", {
+  targetAttribute: "href",
+  duration: 1200,
+  durationRelative: false,
+  durationMin: false,
+  durationMax: false,
+  easing: "easeOutCubic",
+  onScrollEnd: (data) => {},
+  fixedHeader: null,
+});
+smoothScroll.init();
 
-let smoothScroll = new scrollToSmooth('a', {
-    targetAttribute: 'href',
-    duration: 1200,
-    durationRelative: false,
-    durationMin: false,
-    durationMax: false,
-    easing: 'easeOutCubic',
-    onScrollEnd: (data) => { 
-       
-     },
-    fixedHeader: null
-  })
-  smoothScroll.init();
+const toggle = document.querySelector(".toggle");
+const sidebar = document.querySelector(".nav-lists");
+
+toggle.addEventListener("click", function () {
+  sidebar.style.left = "0";
+sidebar.classList.toggle('sidebar')
+  sidebar.classList.contains("sidebar")
+  toggle.classList.add("close")
 
 
+  sidebar.classList.contains("sidebar")
+    ? (toggle.firstElementChild.src = "./images/icons/x.svg")
+    : (toggle.firstElementChild.src = "./images/icons/menu.svg");
+
+    if(!sidebar.classList.contains('sidebar')){
+      sidebar.style.left="-100%";
+    }
+});

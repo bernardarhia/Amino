71c5251035f31b586ef6b82a34ac26a360a4735b
bernardarhia <megamindcooperations@gmail.com>
2020-12-14 20:07:46 +0000

    added animations to 3 sections

--- scripts/animate.js
+++ scripts/animate.js
@@ -28,3 +28,138 @@ sidebar.classList.toggle('sidebar')
       sidebar.style.left="-100%";
     }
 });
+
+
+
+
+// gsap animation
+
+gsap.registerPlugin(ScrollTrigger);
+const tl = gsap.timeline()
+
+tl.to('body', { visibility:'visible'})
+tl.from(['.intro-text .head','.intro-text .para','.intro-text .cta'], {opacity: 0, y: '100px', stagger:'.2',ease: Power2.easeInOut})
+tl.from('.intro-image', { duration:.4, opacity: 0, y: '100px',ease: Power2.easeInOut},'-=.3')
+tl.from('.intro-image .small-card.blue', { duration:.4,opacity: 0, x:'-300px',ease: Power2.easeInOut},"-=.2")
+tl.from('.intro-image  .small-card.yellow', { duration:.4, opacity: 0, x:'300px',ease: Power2.easeInOut}, "-=.2")
+
+
+
+gsap.from(".partners .text p",{
+  scrollTrigger:{
+    trigger: '.partners .text p'
+  },
+  opacity:0,
+ x:'-300px',
+ ease: Power2.easeInOut
+})
+
+gsap.from(".partners .text .line",{
+  scrollTrigger:{
+    trigger: '.partners .text p',
+  },
+  opacity:0,
+ x:'-300px',
+ ease: Power2.easeInOut
+})
+
+gsap.from(".partners .partners-logos",{
+  scrollTrigger:{
+    trigger: '.partners .partners-logos',
+  },
+  delay:.2,
+  opacity:0,
+ x:'300px',
+})
+
+// services animation
+gsap.from(".services-content .card",{
+  scrollTrigger:{
+    trigger: '.services-content .card',
+  },
+opacity: 0,
+y:'50px', stagger:'.2'
+})
+
+gsap.from([".services-content .text-content .title",'.services-content .text-content .para','.services-content .text-content .cta'],{
+  scrollTrigger:{
+    trigger: '.services-content .card',
+    start:'top center'
+  },
+opacity: 0,
+y:'50px', 
+stagger:'.2',
+delay:'1'
+})
+
+gsap.from([".benefits .text-content .title",'.benefits .text-content .para','.benefits .text-content .cta'],{
+  scrollTrigger:{
+    trigger: '.benefits',
+    start:'top center'
+  },
+opacity: 0,
+y:'50px', 
+stagger:'.2',
+})
+
+
+// benefits animation
+
+gsap.from('.benefits .image-content',{
+  scrollTrigger:{
+    trigger: '.benefits',
+    start:'top center'
+  },
+opacity: 0,
+y:'50px', 
+stagger:'.2',
+delay:'.5'
+})
+gsap.from('.benefits .image-content .card',{
+  scrollTrigger:{
+    trigger: '.benefits .image-content',
+    start:'top center'
+  },
+opacity: 0,
+x:'-200px', 
+delay:'.5'
+
+})
+
+
+
+
+
+// benefit two animation
+gsap.from('.benefit-two .image-content',{
+  scrollTrigger:{
+    trigger: '.benefit-two',
+    start:'top center'
+  },
+opacity: 0,
+y:'50px', 
+stagger:'.2',
+})
+gsap.from('.benefit-two .image-content .card',{
+  scrollTrigger:{
+    trigger: '.benefit-two .image-content',
+    start:'top center'
+  },
+opacity: 0,
+x:'-300px', 
+})
+
+gsap.from(['.benefit-two .text-content .title','.benefit-two .text-content .para', '.benefit-two .text-content .cards .card'],{
+  scrollTrigger:{
+    trigger: '.benefit-two',
+    start:'top center'
+  },
+opacity: 0,
+y:'300px', 
+delay:'.3',
+stagger:'.3'
+})
+
+
+
+
